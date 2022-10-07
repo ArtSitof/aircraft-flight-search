45180c551251256aae873f84493a496f570f6f43
AAZaykov <vya8585@gmail.com>
2022-10-07 14:47:08 +0500

    init commit

---
+++ src/main/java/com/naumen/aircraftflightsearch/AircraftFlightSearchApplication.java
@@ -0,0 +1,13 @@
+package com.naumen.aircraftflightsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftFlightSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AircraftFlightSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naumen/aircraftflightsearch/AircraftFlightSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naumen.aircraftflightsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftFlightSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
