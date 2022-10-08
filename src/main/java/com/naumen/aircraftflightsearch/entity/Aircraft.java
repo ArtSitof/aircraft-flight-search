0332d55fed69a8e8aa28ce40e4537069b8b2b2ba
ArtSitof <art-sidof@yandex.ru>
2022-10-08 13:22:34 +0500

    Added Aircraft entity

---
+++ src/main/java/com/naumen/aircraftflightsearch/entity/Aircraft.java
@@ -0,0 +1,20 @@
+package com.naumen.aircraftflightsearch.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "aircrafts")
+@Data
+public class Aircraft {
+
+    @Id
+    @GeneratedValue
+    @Column (name = "id")
+    private Long id;
+
+    @Column(name = "names")
+    private String name;
+
+}
